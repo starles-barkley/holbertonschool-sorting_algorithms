2e683859f72745926d3528327350f399ac242f1b
starles-barkley <144153159+starles-barkley@users.noreply.github.com>
2023-12-20 20:11:43 +0000

    create bubble sort

---
+++ 0-bubble_sort.c
@@ -0,0 +1,28 @@
+#include "sort.h"
+/**
+ * bubble_sort - bubble algorithm to sort array
+ * @array: array to sort
+ * @size: size of array
+ */
+void bubble_sort(int *array, size_t size)
+{
+	size_t i, j;
+	int temp;
+
+	if (array != NULL)
+	{
+		for (i = 0; i < size - 1; i++)
+		{
+			for (j = 0; j < size - 1; j++)
+			{
+				if (array[j] > array[j + 1])
+				{
+					temp = array[j];
+					array[j] = array[j + 1];
+					array[j + 1] = temp;
+					print_array(array, size);
+				}
+			}
+		}
+	}
+}
