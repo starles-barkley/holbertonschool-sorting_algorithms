17f4203a5c804e7a5cb3950659ca5ac09a65d37a
starles-barkley <144153159+starles-barkley@users.noreply.github.com>
2023-12-20 20:33:48 +0000

    revert back to working code

--- 3-quick_sort.c
+++ 3-quick_sort.c
@@ -1,5 +1,4 @@
 #include "sort.h"
-
 /**
  * int_swap - swap integers
  * @first: first element to swap
@@ -7,61 +6,47 @@
  */
 void int_swap(int *first, int *second)
 {
-    int temp = *first;
-    *first = *second;
-    *second = temp;
-}
+	int temp;
 
+	temp = *first;
+	*first = *second;
+	*second = temp;
+}
 /**
- * lomuto_part - implement Lomuto partition
+ * lomuto_part - implement lomuto partition
  * @array: array to partition
- * @low: lowest index in array
- * @high: highest index in array
+ * @low: lowest integer in array
+ * @high: highest integer in array
  * Return: index where partition ended up
+ *
  */
-int lomuto_part(int array[], int low, int high)
+int lomuto_part(int array[], size_t size, int low, int high)
 {
-    int pivot = array[high];
-    int i = (low - 1);
+	int pivot = array[high];
+	int i = low - 1;
+	int j = 0;
 
-    for (int j = low; j <= high - 1; j++)
-    {
-        if (array[j] < pivot)
-        {
-            i++;
-            int_swap(&array[i], &array[j]);
-        }
-    }
-    int_swap(&array[i + 1], &array[high]);
-    return (i + 1);
+	for (j = low; j <= high - 1; j++)
+	{
+		if (array[j] < pivot)
+		{
+			i++;
+			int_swap(&array[i], &array[j]);
+		}
+	}
+	print_array(array, size);
+	int_swap(&array[i + 1], &array[high]);
+	return (i + 1);
 }
-
 /**
- * quicksort_recursive - recursive function for Quick Sort
+ * quick_sort - implemention of quick sort algorithm
  * @array: array to partition and sort
- * @low: lowest index in array
- * @high: highest index in array
- */
-void quicksort_recursive(int array[], int low, int high)
-{
-    if (low < high)
-    {
-        int partition_index = lomuto_part(array, low, high);
-
-        quicksort_recursive(array, low, partition_index - 1);
-        quicksort_recursive(array, partition_index + 1, high);
-    }
-}
-
-/**
- * quick_sort - implementation of Quick Sort algorithm
- * @array: array to sort
  * @size: size of array
  */
 void quick_sort(int *array, size_t size)
 {
-    if (array == NULL || size < 2)
-        return;
-
-    quicksort_recursive(array, 0, size - 1);
+	if (size > 0)
+	{
+		lomuto_part(array, size, 0, size - 1);
+	}
 }
