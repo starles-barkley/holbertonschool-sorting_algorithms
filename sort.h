969f3b52a2498272cef78d395403fdb2a12e13bf
starles-barkley <144153159+starles-barkley@users.noreply.github.com>
2023-12-20 20:13:12 +0000

    create header file

---
+++ sort.h
@@ -0,0 +1,27 @@
+#ifndef SORT_H
+#define SORT_H
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * struct listint_s - Doubly linked list node
+ *
+ * @n: integer stored in the node
+ * @prev: pointer to the previous element of the list
+ * @next: pointer to the next element of list
+ */
+typedef struct listint_s
+{
+	const int n;
+	struct listint_s *prev;
+	struct listint_s *next;
+} listint_t;
+
+void print_array(const int *array, size_t size);
+void print_list(const listint_t *list);
+void bubble_sort(int *array, size_t size);
+void insertion_sort_list(listint_t **list);
+void selection_sort(int *array, size_t size);
+void quick_sort(int *array, size_t size);
+int lomuto_part(int array[], size_t size, int low, int high);
+void int_swap(int *first, int *second);
+#endif
