0a9440ea47c36dc7d55ceb6fe8bda0b923e0a726
starles-barkley <144153159+starles-barkley@users.noreply.github.com>
2023-12-20 20:18:54 +0000

    create select sort

---
+++ 2-selection_sort.c
@@ -0,0 +1,30 @@
+#include "sort.h"
+/**
+ * selection_sort - sort array using selection algorithm
+ * @array: array to be sorted
+ * @size: size of array
+ */
+void selection_sort(int *array, size_t size)
+{
+	size_t i, j, min;
+	int temp;
+
+	for (i = 0; i < size - 1; i++)
+	{
+		min = i;
+		for (j = i + 1; j < size; j++)
+		{
+			if (array[j] < array[min])
+			{
+				min = j;
+			}
+		}
+		if (min != i)
+		{
+			temp = array[min];
+			array[min] = array[i];
+			array[i] = temp;
+			print_array(array, size);
+		}
+	}
+}
