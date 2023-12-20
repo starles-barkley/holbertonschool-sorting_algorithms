338825fa0a98c5ebcdf0d95589b4df1d96d3afae
starles-barkley <144153159+starles-barkley@users.noreply.github.com>
2023-12-20 20:30:42 +0000

    create helper function to test

--- 3-quick_sort.c
+++ 3-quick_sort.c
@@ -13,18 +13,17 @@ void int_swap(int *first, int *second)
 }
 
 /**
- * lomuto_part - implement lomuto partition
+ * lomuto_part - implement Lomuto partition
  * @array: array to partition
- * @size: size of array
  * @low: lowest index in array
  * @high: highest index in array
  * Return: index where partition ended up
  */
-int lomuto_part(int array[], size_t size, int low, int high)
+int lomuto_part(int array[], int low, int high)
 {
     int pivot = array[high];
-    int i = low - 1;
-    
+    int i = (low - 1);
+
     for (int j = low; j <= high - 1; j++)
     {
         if (array[j] < pivot)
@@ -33,20 +32,36 @@ int lomuto_part(int array[], size_t size, int low, int high)
             int_swap(&array[i], &array[j]);
         }
     }
-    print_array(array, size);
     int_swap(&array[i + 1], &array[high]);
     return (i + 1);
 }
 
 /**
- * quick_sort - implementation of quick sort algorithm
+ * quicksort_recursive - recursive function for Quick Sort
  * @array: array to partition and sort
+ * @low: lowest index in array
+ * @high: highest index in array
+ */
+void quicksort_recursive(int array[], int low, int high)
+{
+    if (low < high)
+    {
+        int partition_index = lomuto_part(array, low, high);
+
+        quicksort_recursive(array, low, partition_index - 1);
+        quicksort_recursive(array, partition_index + 1, high);
+    }
+}
+
+/**
+ * quick_sort - implementation of Quick Sort algorithm
+ * @array: array to sort
  * @size: size of array
  */
 void quick_sort(int *array, size_t size)
 {
-    if (size > 1)
-    {
-        lomuto_part(array, size, 0, size - 1);
-    }
+    if (array == NULL || size < 2)
+        return;
+
+    quicksort_recursive(array, 0, size - 1);
 }
